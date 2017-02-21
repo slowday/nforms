d8ad80d6b5006822a24137ddac734350797862e2
John Waweru Wambugu <wambugu.john.waweru@outlook.com>
2017-02-21 06:28:12 +0300

    cleared porting authentication and user model code not yet tested and user main class implementation started

--- src/auth.ts
+++ src/auth.ts
@@ -4,20 +4,51 @@
 
 import * as express from 'express';
 import * as expressJWT from 'express-jwt';
+import * as _ from 'lodash';
+import * as moment from 'moment';
 import { logger as log } from './logger';
+import { UserProfile, UserModel } from './users/UserModel';
 
-export default class Auth {
+export default class Auth extends UserModel {
     /**
      * @docs: 
      *     This is a static class whose domain is authentication and authorization
     */
     static authJWT = expressJWT({
-      secret : <string>process.env.JWT_SECRET,
+      secret : process.env.JWT_SECRET,
       userProperty : 'zushar_auth'
     });
 
-    static getLoggedInUser(request: express.Request, response: express.Response, next: express.NextFunction) {
-        // requires user module
+    // request is type any to suppress error about zushar_auth
+    static getLoggedInUser(request: any, response: express.Response, next: express.NextFunction): void {
+        // query for user details
+        if (_.isEmpty(request.zushar_auth) || _.isNil(request.zushar_auth.id)) {
+            request.zushar_auth.isLoggedin = false;
+            next();
+        }
+
+        super.getOneUser(request.zushar_auth.id)
+            .then((data: UserProfile) => {
+                let age = null;
+                request.zushar_auth.isLoggedin = true;
+                if (!_.isEmpty(data.dob)) {
+                    let dob = moment(data.dob);
+                    let now = moment();
+                    age = now.diff(dob, 'years', true);
+                }
+                //  add user data to request object
+                request.zushar_auth.user_data = {
+                    email: data.email,
+                    gender: data.gender,
+                    phone: data.phone,
+                    name: data.name,
+                    age: (age)
+                };
+                next();
+            })
+            .catch((err: Error) => {
+                next(err);
+            });
     }
     
     static refreshToken(request: express.Request, response: express.Response, next: express.NextFunction) {
--- src/users/UserModel.ts
+++ src/users/UserModel.ts
@@ -2,11 +2,14 @@
  * created by waweru
 */
 
+import * as _ from 'lodash';
+import * as mongoose from 'mongoose';
 import { logger as log } from '../logger';
 import { userModel as db } from './UserSchema';
 
-export interface editedUser extends UserProfile {
+export interface updateNotify {
     done: boolean; 
+    timestamp: Date;
 };
 
 export interface User extends UserProfile{
@@ -55,12 +58,13 @@ export class UserModel {
                 return Promise.reject(err);
             });
     }
-    protected loginUser(params: AuthData): Promise<User> {
+    protected authenticateUser(params: AuthData): Promise<User> {
         let { email, phone, password } = params;
         return db
             .findOne({ email, phone })
             .exec()
             .then((data) => {
+                // check if user exist
                 if (!data) {
                     let date = new Date();
                     let err: Error = new Error(`${date}:: account not found`);
@@ -70,6 +74,7 @@ export class UserModel {
                 return Promise.resolve(data);
             })
             .then((data) => {
+                //#then check if password entered is valid
                 if (!data.validatePassword(params.password)) {
                     let date = new Date();
                     let err: Error = new Error(`${date}:: password is incorrect`);
@@ -79,6 +84,7 @@ export class UserModel {
                 return Promise.resolve(data);
             })
             .then((data) => {
+                //# then check if user is marked as disabled
                 if (data.deletion) {
                     let date = new Date();
                     let err: Error = new Error(`${date.toDateString()}:: user-account is disabled`);
@@ -88,23 +94,79 @@ export class UserModel {
                 return Promise.resolve(data);
             })
             .then((data) => {
+                // #then return user
                let loggedInUser =  { user: data, jwt_token: data.generateJWT() };
                delete loggedInUser.user.password; // remove user password
                return Promise.resolve(loggedInUser);
             })
             .catch((err: Error) => {
+                // otherwise return error if something goes wrong
                 log.error(err);
                 return Promise.reject(err);
             });
     }
-    protected updateUser(): editedUser {
-        return <editedUser>{};
-    }
-    protected diableUser(): editedUser {
-        return <editedUser>{};
+    protected updateUser(id: string, params: AuthData, updates: any): Promise<updateNotify> {
+        /**
+         * @docs: 
+         *     This method will be used to disable user accounts as well
+        */
+        let updatesCopy = _.assign({}, updates);
+        let { email, phone, password } = params;
+        let _id = <mongoose.Types.ObjectId>mongoose.Types.ObjectId(id);
+        
+        // authenticate user first
+        return this.authenticateUser(params)
+            .then((data) => {
+                //#then hash the password if it is part of the updates to be done
+                if (!_.isEmpty(updates.password)) {
+                    updatesCopy = _.assign({}, updatesCopy, {
+                        password: db.setPassword(updates.password)
+                    });
+                }
+                return Promise.resolve(updatesCopy);
+            })
+            .then(() => {
+                // #then run the update
+                return db.update({ _id, email, phone }, { $set: updatesCopy },
+                {  
+                    safe: true,
+                    multi: false,
+                    runValidators: true
+                }).exec();
+            })
+            .then((updated) => {
+                if (updated.ok === 1 && updated.nModified === 1) {
+                    return Promise.resolve({
+                        done: true,
+                        timestamp: new Date()
+                    });
+                }
+                else {
+                    return Promise.resolve({
+                        done: false,
+                        timestamp: new Date()
+                    });
+                }
+            })
+            .catch((err: Error) => {
+                // otherwise return error if something goes wrong
+                log.error(err);
+                return Promise.reject(err);
+            });
     }
     
-    static getOneUser(): UserProfile {
-        return <UserProfile>{};
+    static getOneUser(id: string): Promise<UserProfile> {
+        let _id = <mongoose.Types.ObjectId>mongoose.Types.ObjectId(id);
+        return db
+            .findById(_id)
+            .select({ 'password': 0 })
+            .exec()
+            .then((data) => {
+                return Promise.resolve(data);
+            })
+            .catch((err: Error) => {
+                log.error(err);
+                return Promise.reject(err);
+            });
     }
 }
