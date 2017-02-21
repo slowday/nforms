94bc7c2d39117e4389110c6608b84d85d2f16601
John Waweru Wambugu <wambugu.john.waweru@outlook.com>
2017-02-21 07:19:03 +0300

    cleared porting forms schema implementation and forms model for forms data manipulation porting began

--- src/forms/FormsModel.ts
+++ src/forms/FormsModel.ts
@@ -1,3 +1,11 @@
 /**
  * created by waweru
 */
+
+import * as _ from 'lodash';    
+import { logger as log } from '../logger';
+import { formsModel as db } from './FormsSchema';
+
+export class FormsModel {
+
+}
